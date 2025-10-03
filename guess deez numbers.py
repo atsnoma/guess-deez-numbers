a8470229960b879836cf125b83de62be63c7a883
atsnoma <misterleafjuice@gmail.com>
2025-10-02 17:37:44 -0700

    Add files via upload

--- guess deez numbers.py
+++ guess deez numbers.py
@@ -10,20 +10,20 @@ playerHealth = 5
 directionDetection = 0
 roundOffTokens = 0
 skipPassTokens = 0
-level = 2
+level = 1
 
 #This controls level changes and reports resources to the player between rounds.
-def levelManagement():
+def levelManagement(level, playerHealth, skipPassTokens, roundOffTokens, directionDetection):
     level += 1
     playerHealth += 1+level
     print("You have %i health, as well as %i Skip Tokens and %i Rounding Tokens. Your Detection Level is at %i." % (playerHealth, skipPassTokens, roundOffTokens, directionDetection))
-    checkupTokens()
-    checkupUpgrades()
+    roundOffTokens, skipPassTokens = checkupTokens(level, roundOffTokens, skipPassTokens)
+    checkupUpgrades(level)
     input("Press Any Key to Continue...")
-    startGame()
+    startGame(level, playerHealth, totScore)
 
 #This handles restocking of tokens
-def checkupTokens():
+def checkupTokens(level, roundOffTokens, skipPassTokens):
     if level < 4:
         return
     elif level == 4:
@@ -38,8 +38,7 @@ def checkupTokens():
         skipInput = input("Would you like any more Skip Tokens? The game will get harder if you do! (Y/N)")
         if skipInput == "Y" :
             skipPassTokens += math.floor(level/2)
-            minimumNumber -= 2*level
-            maximumNumber += 2*level
+            level += 1
             print("Added " + math.floor(level/2) + " Skip Tokens.")
             pass
         elif skipInput == "N" :
@@ -53,17 +52,16 @@ def checkupTokens():
         roundInput = input("Would you like any more Round Tokens? The game will get harder if you do! (Y/N)")
         if roundInput == "Y" :
             roundOffTokens += math.floor(level/2)
-            minimumNumber -= 2*level
-            maximumNumber += 2*level
+            level += 1
             print("Added " + math.floor(level/2) + " Round Tokens.")
-            return
+            return roundOffTokens, skipPassTokens
         elif roundInput == "N" :
-            return
+            return roundOffTokens, skipPassTokens
         else:
             print("Please type either \'Y\' or \'N\'.")
     except:
         print ("No tokens today.")
-        return
+        return roundOffTokens, skipPassTokens
 
 def checkupUpgrades():
     if level < 5:
@@ -87,23 +85,25 @@ def checkupUpgrades():
         return
 
 #This starts the next round of the game.
-def startGame():
-    for i in range (level):
-        playerHealth, minimumNumber, maximumNumber, totScore = nextNumber(playerHealth, minimumNumber, maximumNumber, totScore)
+def startGame(level, playerHealth, totScore):
+    for i in range (level+1):
+        playerHealth, totScore = nextNumber(level, playerHealth, totScore, directionDetection)
         if playerHealth == 0:
             break
-    levelManagement()
+    else:
+        levelManagement(level, playerHealth, skipPassTokens, roundOffTokens, directionDetection)
 
 #This function generates a random number for each battle.
-def generateNumber(minimumNumber, maximumNumber):
-    generatedNumber = random.randrange(minimumNumber, maximumNumber)
-    return generatedNumber
-
-#This function handles events during a correct guess.
-def correctGuess(targetNumber, minimumNumber, maximumNumber, totScore):
-    print("That is correct! The number was", targetNumber, "!")
+def generateNumber(level):
+    minimumNumber = 0
+    maximumNumber = 10
     minimumNumber -= 2*level
     maximumNumber += 2*level
+    return random.randrange(minimumNumber, maximumNumber)
+
+#This function handles events during a correct guess.
+def correctGuess(targetNumber, totScore):
+    print("That is correct! The number was", targetNumber, "!")
     totScore += 1+level
     return totScore
 
@@ -143,18 +143,19 @@ def hintNumber():
             print("The number is not divisble by numbers 2 through 9!")
     
 #This function handles each battle.
-def nextNumber(playerHealth, minimumNumber, maximumNumber, totScore):
-    targetNumber = int(generateNumber())
+def nextNumber(level, playerHealth, totScore, directionDetection):
+    targetNumber = int(generateNumber(level))
     print(targetNumber)
     if directionDetection > 0:
         hintNumber()
     while playerHealth > 0:
         playerGuess = int(input("Take a Guess: "))
         if playerGuess == targetNumber:
-            totScore = correctGuess(targetNumber, minimumNumber, maximumNumber, totScore)
-            return playerHealth, minimumNumber, maximumNumber, totScore
+            totScore = correctGuess(targetNumber, totScore)
+            return playerHealth, totScore
         (playerHealth, totScore) = incorrectGuess(playerHealth, totScore)
-
+        if playerHealth == 0:
+            return playerHealth, totScore
 #This function handles the end of the game.
 def gameOver(totScore):
     print("You are out of guesses. Everything is lost.")
@@ -166,4 +167,4 @@ def gameOver(totScore):
 #This starts the first round
 print("Welcome to Guess Deez Numbers!")
 print("Your job is to guess a number between 0 and 10, but it will get harder over time!")
-startGame()
+startGame(level, playerHealth, totScore)
